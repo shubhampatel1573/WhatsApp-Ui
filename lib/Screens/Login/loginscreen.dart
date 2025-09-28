84086397d85a1b3ea2334df6d41627dcf6e15201
Shubham Patel <shubhampatel1573@gmail.com>
2025-09-28 19:15:40 +0530

    second page conpleted

--- lib/Screens/Login/loginscreen.dart
+++ lib/Screens/Login/loginscreen.dart
@@ -1,11 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_app/Widgets/uihelper.dart';
 
-class Loginscreen extends StatelessWidget {
-  const Loginscreen({super.key});
+class Loginscreen extends StatefulWidget {
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  TextEditingController phoneController = TextEditingController();
+  String selectedcountry = "India";
+
+  List<String> countries = [
+    "India",
+    "America",
+    "Africa",
+    "Italy",
+    "Germany",
+    "China",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.white,
+      //   title: UiHelper.customText(
+      //     text: "Enter your phone number",
+      //     height: 16,
+      //     color: Color(0XFF00A884),
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          SizedBox(height: 80),
+          Center(
+            child: UiHelper.customText(
+              text: "Enter your phone number",
+              height: 20,
+              color: Color(0XFF00A884),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 30),
+          UiHelper.customText(
+            text: "WhatsApp will need to verify your phone",
+            height: 14,
+          ),
+          SizedBox(height: 3),
+          UiHelper.customText(
+            text: "number. Carrier chnage may apply",
+            height: 14,
+          ),
+          SizedBox(height: 10),
+          UiHelper.customText(
+            text: "What's my number?",
+            height: 14,
+            color: Color(0XFF00A884),
+          ),
+          SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.only(left: 60, right: 60),
+            child: DropdownButtonFormField(
+              items: countries.map((String country) {
+                return DropdownMenuItem(
+                  value: country,
+                  child: Text(country.toString()),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedcountry = value!;
+                });
+              },
+              value: selectedcountry,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0XFF00A884)),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Color(0XFF00A884)),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 50,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "+91",
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0XFF00A884)),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0XFF00A884)),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0XFF00A884)),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              SizedBox(
+                width: 185,
+                child: TextField(
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0XFF00A884)),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: UiHelper.customButton(
+        callback: () {},
+        buttonname: "Next",
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
-  
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0XFF00A884)),
         useMaterial3: false,
+        fontFamily: "Regular",
       ),
       home: Splashscreen(),
     );
